d2974dc838429d4ff41e030bfa9f28aed44edf61
klynkin_kirill <klynkin.kirill@yandex.ru>
2019-11-16 00:47:36 +0300

    16.11

--- main.c
+++ main.c
@@ -4,28 +4,31 @@
 	uint16_t counter2=0;
 void TIM2_IRQHandler(void)
 {
-	
 		TIM2->SR=~TIM_SR_UIF;
 		if(counter<999)
 		counter++;
-		else	
-		counter=0;
+		else
+		{
+		TIM2->CR1 = 0;
+		NVIC_DisableIRQ(TIM2_IRQn);
+		}
 }
+
 void EXTI0_IRQHandler()
 {
-		
-				EXTI->PR |= EXTI_PR_PR0;
-				NVIC_DisableIRQ(TIM2_IRQn);
-
-		
+		EXTI->PR |= EXTI_PR_PR0;
+		TIM2->CR1 = 0;
+		NVIC_DisableIRQ(TIM2_IRQn);
 }
+
 void EXTI1_IRQHandler()
 {
-		
-			EXTI->PR |= EXTI_PR_PR0;
-			NVIC_EnableIRQ(TIM2_IRQn);
-
-		
+		EXTI->PR |= EXTI_PR_PR0;
+		TIM2->CR1 |= TIM_CR1_CEN;
+		NVIC_EnableIRQ(TIM2_IRQn);
+		if(counter==999)
+		counter=0;	
+	
 }
 
 void initBtn()
@@ -34,21 +37,17 @@ void initBtn()
 	GPIOB->CRL &= ~GPIO_CRL_CNF0_0;
 	GPIOB->CRL |= GPIO_CRL_CNF0_1;
 	GPIOB->CRL	&= ~GPIO_CRL_MODE0 ;
-        //подтягиваем к питанию
+  //подтягиваем к питанию
 	GPIOB->BSRR |= GPIO_BSRR_BR0;
-	
-         //выбор порта и пина для внешнего прерывания
+  //выбор порта и пина для внешнего прерывания
 	 AFIO->EXTICR [0] |= AFIO_EXTICR1_EXTI0_PB;
-         //по спадающему фронту
+  //по спадающему фронту
 	 EXTI->FTSR |= EXTI_FTSR_TR0;
-         //устанавливаем маску
+  //устанавливаем маску
    EXTI->IMR |= EXTI_IMR_MR0;
-         //разрешаем прерывания  EXTI0
-	
+  //разрешаем прерывания  EXTI0
 	NVIC_EnableIRQ(EXTI0_IRQn);
 	NVIC_SetPriority(EXTI0_IRQn,1);
-	
-		
 }
 
 
@@ -59,36 +58,32 @@ void initBtn2()
 	GPIOB->CRL &= ~GPIO_CRL_CNF1_0;
 	GPIOB->CRL |= GPIO_CRL_CNF1_1;
 	GPIOB->CRL	&= ~GPIO_CRL_MODE1 ;
-        //подтягиваем к питанию
+  //подтягиваем к питанию
 	GPIOB->BSRR |= GPIO_BSRR_BR1;
-	
-         //выбор порта и пина для внешнего прерывания
+  //выбор порта и пина для внешнего прерывания
 	 AFIO->EXTICR [0] |= AFIO_EXTICR1_EXTI1_PB;
-         //по спадающему фронту
+  //по спадающему фронту
 	 EXTI->FTSR |= EXTI_FTSR_TR1;
-         //устанавливаем маску
+  //устанавливаем маску
    EXTI->IMR |= EXTI_IMR_MR1;
-         //разрешаем прерывания  EXTI1
-	
+  //разрешаем прерывания  EXTI1
 	NVIC_EnableIRQ(EXTI1_IRQn);
 	NVIC_SetPriority(EXTI1_IRQn,1);
-	
-		
 }
 
-int main(void)
+void initTim2()
 {
-//	RCC->CR=RCC_CR_HSEON;
-//	while((RCC->CR=RCC_CR_HSERDY == RESET));
   RCC->APB1ENR|=RCC_APB1ENR_TIM2EN;
-//	RCC->CR&=~RCC_CR_PLLON;
 	TIM2->PSC = 36000-1; //2кГц
 	TIM2->ARR = 2000-1;
 	TIM2->DIER |= TIM_DIER_UIE;
 	TIM2->CR1 |= TIM_CR1_CEN;
-	NVIC_EnableIRQ(TIM2_IRQn);
+	NVIC_DisableIRQ(TIM2_IRQn);
+}
+
+void initGPIO()
+{
 	
-	RCC->APB2ENR|=RCC_APB2ENR_IOPCEN;
 	RCC->APB2ENR|=RCC_APB2ENR_IOPAEN;
 	GPIOA->CRL &= 0xF0000000;
 	GPIOA->CRL |= 0x03333333;
@@ -102,33 +97,23 @@ int main(void)
 	GPIOA->CRH &= ~(GPIO_CRH_CNF9 | GPIO_CRH_MODE9);	
 	GPIOA->CRH |= (GPIO_CRH_MODE9_1);
 	GPIOA->BSRR=GPIO_BSRR_BR9;
+}
+
+int main(void)
+{
+	initTim2();	
+	initGPIO();
 	initBtn();
 	initBtn2();
+
 	
 	while(1)
-	{
-		
-//		GPIOA->ODR &= 0xFF80;
-//		GPIOA->BSRR=GPIO_BSRR_BS9;
-//		GPIOA->BSRR=GPIO_BSRR_BR10;
-//		GPIOA->ODR |=0x003F;
-//		delay(10);
-//		GPIOA->ODR &= 0xFF80;
-//		GPIOA->BSRR=GPIO_BSRR_BS10;
-//		GPIOA->BSRR=GPIO_BSRR_BR9;
-//		GPIOA->ODR |=0x005B;
-//		delay(10);
-		
-
-   // delay(10000000);
-							
-				
-				
-							GPIOA->ODR &= 0xFF80;
-							counter2=counter%10;
-							GPIOA->BSRR=GPIO_BSRR_BS9;
-							GPIOA->BSRR=GPIO_BSRR_BS10;
-							GPIOA->BSRR=GPIO_BSRR_BR8;
+{
+	GPIOA->ODR &= 0xFF80;				//скидываем значение порта
+	counter2=counter%10;				//берем последнюю цирфу
+	GPIOA->BSRR=GPIO_BSRR_BS9;	//выключаем 2 регистр
+	GPIOA->BSRR=GPIO_BSRR_BS10;	//выключаем 3 регистр
+	GPIOA->BSRR=GPIO_BSRR_BR8;	//включаем 1 регистр
 								
 						switch (counter2)
 						{
@@ -234,10 +219,10 @@ int main(void)
 				counter2=counter2%10;	
 				else continue;
 							
-							GPIOA->ODR &= 0xFF80;
-							GPIOA->BSRR=GPIO_BSRR_BS8;			
-							GPIOA->BSRR=GPIO_BSRR_BS9;
-							GPIOA->BSRR=GPIO_BSRR_BR10;
+				GPIOA->ODR &= 0xFF80;
+				GPIOA->BSRR=GPIO_BSRR_BS8;			
+				GPIOA->BSRR=GPIO_BSRR_BS9;
+				GPIOA->BSRR=GPIO_BSRR_BR10;
 
 				switch (counter2)
 						{
@@ -280,13 +265,7 @@ int main(void)
 							case 9:
 							GPIOA->ODR |=0x006F;
 							break;
-					
 				}
-				
-				
-														
-
-				
 	}
 }
 
